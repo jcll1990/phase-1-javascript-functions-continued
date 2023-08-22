e8be93099ba4977b4ab96e17969cb792601c39cd
Juan Carlos Lopez Larco <Jclopezlarco@gmail.com>
2023-08-21 22:41:41 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(a) {
+    if (a === "bathe my dog") {
+    return "This Saturday, I want to bathe my dog!"
+    }
+    else {    return "This Saturday, I want to roller-skate!"
+}
+}
+
+function mondayWork(b) {
+    if (b === "work from home") {
+        return "This Monday, I will work from home."
+        }
+        else {   
+    return "This Monday, I will go to the office."
+}
+}
+
+function wrapAdjective(c) {
+    return function(texto) {
+        return "You are " + c + texto + c + "!"
+    }
+}
+
